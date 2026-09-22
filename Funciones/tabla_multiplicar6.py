0e73bbeacb3c72dfeb101a85286380a349e85ed6
Mauricioam <mauricioam91@gmail.com>
2026-09-22 01:32:32 +0000

    add: tabla de multiplicar añadida a función

---
+++ Funciones/tabla_multiplicar6.py
@@ -0,0 +1,20 @@
+import aux_funcion as auxfn
+def num_input():
+    valid_data = False
+    while not valid_data:
+        print("Ingresa un número positivo")
+        user_num_input = input()
+        if auxfn.check_num_data(user_num_input):
+            valid_data = True
+            user_num_input = int(user_num_input)
+        else:
+            print("Error: el número no es correcto")
+    return user_num_input
+
+def tabla_multiplicar(num):
+    print("=== Tabla multiplicar ===")
+    for n in range(1,11):
+        print(f"{n} x {num} = {n*num}")
+
+
+
