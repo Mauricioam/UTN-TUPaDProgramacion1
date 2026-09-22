e1af747522cdd20420cbdfdc72108f0d452a9f9a
Mauricioam <mauricioam91@gmail.com>
2026-09-22 00:19:09 +0000

    add: ejercicio area/perimeto del circulo

--- Funciones/area_circulo_4.py
+++ Funciones/area_circulo_4.py
@@ -1,13 +1,16 @@
 import aux_funcion as auxfun
 
 def request_radio():
-    print("Ingresa el radio del círculo: ")
-    user_radio = input()
-    # aca falta validar el dato de q sea un float
+
+    # función para tomar el dato del radio con validaciones.
+    # retorna el valor del radio en tipo float
     valid_data = False
     while not valid_data:
+        print("Ingresa el radio del círculo: ")
+        user_radio = input()
         if auxfun.check_num_data(user_radio):
             valid_data = True
+            user_radio = float(user_radio)
         else:
             print("Error: Ingrese un número valido positivo.")
     return user_radio
@@ -15,6 +18,10 @@ def request_radio():
 radio = request_radio()
 
 def calcular_area_circulo(radio):
-    return 2 * 3.14 * radio
+    print(f"El área del circulo es: {(3.14 * radio):.2f}") 
+
+def calcular_perimetro_circulo(radio):
+    print(f"El perímetro del circulo es: {(2 * 3.14 * radio):.2f}") 
+
 # P = 2 * PI * R
 # A = PI * R
--- Funciones/main.py
+++ Funciones/main.py
@@ -1,17 +1,25 @@
-import hola_mundo_1 as fn
-import saludar_usuario_2 as fn
+import hola_mundo_1 as fn1
+import saludar_usuario_2 as fn2
 import aux_funcion as auxfun
-import info_personal_3 as fn
+import info_personal_3 as fn3
+import area_circulo_4 as fn4
 # Ejercicio 1: imprimir hola mundoS
 
-fn.imprimir_hola_mundo()
+fn1.imprimir_hola_mundo()
 
 #Ejercicio 2: Saludo personalizado del usuario
 
-fn.saludar_usuario("Mauricio")
+fn2.saludar_usuario("Mauricio")
 
 # Ejercicio 3: información personal. Se pide datos al usuario y muestra en pantalla
 
 nombre,apellido,residencia,edad = auxfun.pedir_datos()
 
-fn.informacion_personal(nombre,apellido,residencia,edad)
+fn3.informacion_personal(nombre,apellido,residencia,edad)
+
+#Ejercicio 4: area y perimetro de un circulo con el ingreso del usuario.
+radio = fn4.request_radio()
+
+fn4.calcular_perimetro_circulo(radio)
+
+fn4.calcular_area_circulo(radio)
